0a88c0e5c09012eb85b82214f3289addc3b0a28f
nixon-crypto <leandro.souza2017@hotmail.com>
2025-08-20 08:21:05 -0300

    Delete JavaScript directory

--- JavaScript/Aula4/scriptDesafio.js
+++
@@ -1,9 +0,0 @@
-alert('Bem vindo ao gerenciador de tarefas')
-let tarefa1 = prompt('Digite a primeira tarefa:')
-let tarefa2 = prompt('Digite a segunda tarefa:')
-let tarefa3 = prompt('Digite a terceira tarefa:')
-
-let listaDeTarefas = [tarefa1, tarefa2, tarefa3]
-
-
-console.log(listaDeTarefas);
